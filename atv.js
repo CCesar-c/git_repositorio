4a61831294a3890e9be06513b85e50b1caa36aff
DaviGrah2 <davigrah2010@gmail.com>
2026-03-17 16:26:38 -0300

    Desafios e questoes 7 8 9

--- atv.js
+++ atv.js
@@ -1,34 +1,60 @@
 // Atividade 1 
 
-    // let caderno = 12.50
-    // let quantidade = prompt('Qual a quantidade de cadernos que você deseja comprar?')
+// let caderno = 12.50
+// let quantidade = prompt('Qual a quantidade de cadernos que você deseja comprar?')
 
-    // alert('o total a ser pago é R$' + caderno * quantidade )
+// alert('o total a ser pago é R$' + caderno * quantidade )
 
 // Atividade 2
 
-    // let camisa_fabrica = 25.00
-    // let camisa_loja = 40.00
-    // let quantidade = prompt('Quantas camisas foram vendidas?')
+// let camisa_fabrica = 25.00
+// let camisa_loja = 40.00
+// let quantidade = prompt('Quantas camisas foram vendidas?')
 
-    // let total = camisa_loja * quantidade
-    // let lucro = total - (camisa_fabrica * quantidade)
+// let total = camisa_loja * quantidade
+// let lucro = total - (camisa_fabrica * quantidade)
 
-    // alert('O total de lucro é R$' + lucro)
-    
+// alert('O total de lucro é R$' + lucro)
+
 // Atividade 3
 
-    // let preco_original = prompt('Qual o preço do produto?')
+// let preco_original = prompt('Qual o preço do produto?')
 
-    // let preco_desconto = preco_original * 0.90
+// let preco_desconto = preco_original * 0.90
 
-    // alert('O preço do produto com desconto é R$' + preco_desconto)v]
+// alert('O preço do produto com desconto é R$' + preco_desconto)v]
+
+//Atividade 7
+// let estoque = 120;
+// let total_vendido = Number(prompt('Quanto você vendeu hoje?'));
+// let total_estoque = estoque - total_vendido;
+
+// if (total_vendido > total_estoque) {
+//     alert("erro ao verificar (estoque) - (total_vendido)")
+// }
+// else{
+//     alert('Estoque restante : ' + total_estoque);
+// };
+
+//Atividade 8
+// let preco_atual = Number(prompt("Valor do produto :"));
+// let preco_novo = preco_atual * 1.15;
+// let diferença_valor = preco_novo - preco_atual;
+
+// alert("Valores após reajuste :" + preco_atual + preco_novo + diferença_valor);
+
+//Atividade 9
+// let valor_compra = number(prompt("Valor da compra :"));
+// let valor_parcela = valor_compra / parcela;
+// let parcela = number(prompt("Parcelar em quantas vezes :"));
+
+// alert("Valor final :" + valor_compra + parcela + valor_parcela);
 
 // Atividade 10
 
-    // let investimento_inicial = prompt('Qual o valor do investimento inicial?')
-    // let meses = prompt('Quantos meses o dinheiro ficará investido?')
+// let investimento_inicial = prompt('Qual o valor do investimento inicial?')
+// let meses = prompt('Quantos meses o dinheiro ficará investido?')
 
-    // let valor_final = investimento_inicial * (1 + 0.02) ** meses
+// let valor_final = investimento_inicial * (1 + 0.02) ** meses
 
-    // alert('O valor final do investimento é R$' + valor_final.toFixed(2))
+// alert('O valor final do investimento é R$' + valor_final.toFixed(2))
