7afd28b459d514a4c08dac0a54a09f8e266398d7
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2026-03-17 16:02:25 -0300

    atv 1,2 ,3 e 10 e html

---
+++ atv.js
@@ -0,0 +1,34 @@
+// Atividade 1 
+
+    // let caderno = 12.50
+    // let quantidade = prompt('Qual a quantidade de cadernos que você deseja comprar?')
+
+    // alert('o total a ser pago é R$' + caderno * quantidade )
+
+// Atividade 2
+
+    // let camisa_fabrica = 25.00
+    // let camisa_loja = 40.00
+    // let quantidade = prompt('Quantas camisas foram vendidas?')
+
+    // let total = camisa_loja * quantidade
+    // let lucro = total - (camisa_fabrica * quantidade)
+
+    // alert('O total de lucro é R$' + lucro)
+    
+// Atividade 3
+
+    // let preco_original = prompt('Qual o preço do produto?')
+
+    // let preco_desconto = preco_original * 0.90
+
+    // alert('O preço do produto com desconto é R$' + preco_desconto)v]
+
+// Atividade 10
+
+    // let investimento_inicial = prompt('Qual o valor do investimento inicial?')
+    // let meses = prompt('Quantos meses o dinheiro ficará investido?')
+
+    // let valor_final = investimento_inicial * (1 + 0.02) ** meses
+
+    // alert('O valor final do investimento é R$' + valor_final.toFixed(2))
